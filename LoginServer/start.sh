d1095b9f68fcaba02afce8002a4c99632c70f058
xubo <2062152083@qq.com>
2018-03-24 00:05:55 +0800

    update

---
+++ LoginServer/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+erl +K true +P 1048576 +Q 1048576 +t 4194304 -name biganans_login@127.0.0.1 -setcookie biganans_login -pa ebin deps/ebin config config/app -s user_default ss -detached
+
